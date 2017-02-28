afc85bfd90de282c9e31540ff8319a9471f5837c
mldangelo <michael.l.dangelo@gmail.com>
2017-02-28 04:59:29 -0800

    updated post descriptons

--- app/data/posts.js
+++ app/data/posts.js
@@ -2,20 +2,9 @@ const data = [
   {
     path: '/blog/first',
     title: 'First Post',
-    summary: 'Welcome to my blog.',
-    date: '2017-02-19',
-  },
-  {
-    path: '/blog/second',
-    title: 'Second Post',
-    summary: 'Welcome to my blog2.',
-    date: '2017-02-20',
-  },
-  {
-    path: '/blog/third',
-    title: 'Third Post',
-    summary: 'Welcome to my blog3.',
-    date: '2017-02-22',
+    summary: 'I had to pick something. Here\'s a quick introduction and ' +
+    'ethos of what this blog will attempt to accomplish.',
+    date: '2017-02-28',
   },
 ];
 
